58fbf5e9a2108b7acf0c9279826e491fdcdb6d27
Radek Kočka <kocka.radek@gmail.com>
2025-05-11 14:45:51 +0200

    Add TaskDetailsStyle Add Listbox deselection

--- Personal Task Manager/Views/MainWindow.xaml.cs
+++ Personal Task Manager/Views/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using Personal_Task_Manager.ViewModel;
+﻿using Personal_Task_Manager.Models;
+using Personal_Task_Manager.ViewModel;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace Personal_Task_Manager
 {
@@ -12,6 +16,23 @@ namespace Personal_Task_Manager
         {
             InitializeComponent();
             DataContext = new MainViewModel();
+        }
+
+        private void ListBox_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is ListBox listBox)
+            {
+                // Check if the click is on an empty space
+                var clickedItem = VisualTreeHelper.HitTest(this, e.GetPosition(this));
+                if (clickedItem?.VisualHit is not FrameworkElement element || element.DataContext is not TaskItem)
+                {
+                    // Set SelectedTask to null
+                    if (DataContext is ViewModel.MainViewModel viewModel)
+                    {
+                        viewModel.SelectedTask = null!;
+                    }
+                }
+            }
         }
     }
 }
