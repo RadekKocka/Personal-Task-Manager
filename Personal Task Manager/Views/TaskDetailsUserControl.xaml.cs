e7ac2c26a7dac27237c7406c236c6b75064a596a
Radek Kočka <kocka.radek@gmail.com>
2025-05-22 00:09:01 +0200

    Task details extracted into user control

---
+++ Personal Task Manager/Views/TaskDetailsUserControl.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Personal_Task_Manager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Personal_Task_Manager.Views
+{
+    /// <summary>
+    /// Interaction logic for TaskDetailsUserControl.xaml
+    /// </summary>
+    public partial class TaskDetailsUserControl : UserControl
+    {
+        public TaskDetailsUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
