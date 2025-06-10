d9ccb2e7483f711a0c079e1c2e5ef32c82d016e2
Radek Kočka <kocka.radek@gmail.com>
2025-06-10 12:35:26 +0200

    added filter button for completed tasks. subject to change

--- Personal Task Manager/Models/Enums/TaskState.cs
+++ Personal Task Manager/Models/Enums/TaskState.cs
@@ -1,30 +1,33 @@
 ﻿namespace Personal_Task_Manager.Models.Enums
 {
+    [Flags]
     public enum TaskState
     {
         /// <summary>
         /// Task that is ongoing.
         /// </summary>
-        InProgress,
+        InProgress = 1,
         /// <summary>
         /// Completed task.
         /// </summary>
-        Complete,
+        Complete = 1 << 1,
         /// <summary>
         /// Task not yet started.
         /// </summary>
-        NotStarted,
+        NotStarted = 1 << 2,
         /// <summary>
         /// Task that is overdue.
         /// </summary>
-        Late,
+        Late = 1 << 3,
         /// <summary>
         /// Archived task.
         /// </summary>
-        Archived,
+        Archived = 1 << 4,
         /// <summary>
         /// Deleted task
         /// </summary>
-        Deleted
+        Deleted = 1 << 5,
+
+        All = InProgress | Complete | NotStarted | Late | Archived | Deleted
     }
 }
