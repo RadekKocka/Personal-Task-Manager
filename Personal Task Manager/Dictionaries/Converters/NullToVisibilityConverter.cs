54972413004c89175d79a4ae3fa6bf69ebf6c2af
Radek Kočka <kocka.radek@gmail.com>
2025-06-11 21:58:24 +0200

    Added NullTovisbilityConverter Changed colors. adjusted backgrounds Moved converters to global app.xaml

---
+++ Personal Task Manager/Dictionaries/Converters/NullToVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Personal_Task_Manager.Dictionaries.Converters
+{
+    internal class NullToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter is not null && parameter is string param)
+            {
+                if (param.Equals("Inverse", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return value == null ? System.Windows.Visibility.Visible : System.Windows.Visibility.Collapsed;
+                }
+            }
+            return value == null ? System.Windows.Visibility.Collapsed : System.Windows.Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
